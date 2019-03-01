c6f461923df0633233186ae41b39856d9a9fee41
kik4 <kik4.contact@gmail.com>
2019-03-02 04:42:11 +0900

    .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,13 +2,8 @@ const rulesDirPlugin = require("eslint-plugin-rulesdir");
 rulesDirPlugin.RULES_DIR = "rules";
 
 module.exports = {
-  env: {
-    browser: true,
-    commonjs: true,
-    es6: true
-  },
   parserOptions: {
-    ecmaVersion: 2018
+    ecmaVersion: 6
   },
   plugins: ["rulesdir"],
   rules: {
