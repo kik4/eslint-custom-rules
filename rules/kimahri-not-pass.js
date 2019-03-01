f7968b479f9478d5472ed351e770befee6bcea2c
kik4 <kik4.contact@gmail.com>
2019-03-02 04:20:55 +0900

    style

--- rules/kimahri-not-pass.js
+++ rules/kimahri-not-pass.js
@@ -1,16 +1,19 @@
 "use strict";
 
-module.exports = function(context) {
-  return {
-    Literal: function(node) {
-      if (
-        typeof node.value == "string" &&
-        node.value.indexOf("キマリ") !== -1
-      ) {
-        context.report({ node: node, message: "キマリは通さない" });
+module.exports = {
+  meta: {
+    schema: []
+  },
+  create: function(context) {
+    return {
+      Literal: function(node) {
+        if (
+          typeof node.value == "string" &&
+          node.value.indexOf("キマリ") !== -1
+        ) {
+          context.report({ node: node, message: "キマリは通さない" });
+        }
       }
-    }
-  };
+    };
+  }
 };
-
-module.exports.schema = [];
